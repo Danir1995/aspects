689cfdb3da56392de8514bfce408e45fdd26f453
DanirPT <danir.hucaev@gmail.com>
2025-01-04 00:45:30 +0000

    Updated ExceptionLogDto using lombok

--- src/main/java/com/codility/aop/log/ExceptionLogDto.java
+++ src/main/java/com/codility/aop/log/ExceptionLogDto.java
@@ -1,3 +1,18 @@
 package com.codility.aop.log;
 
-public record ExceptionLogDto(String className, String methodName, Throwable exception) {}
+
+import lombok.Data;
+
+@Data
+public class ExceptionLogDto {
+    private String className;
+    private String methodName;
+    private Throwable exception;
+
+    public ExceptionLogDto(String className, String methodName, Throwable exception) {
+        this.className = className;
+        this.methodName = methodName;
+        this.exception = exception;
+    }
+
+}
