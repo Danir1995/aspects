b31e1d4691fcc52a12df9e24610629fc60ad0e1d
DanirPT <danir.hucaev@gmail.com>
2025-01-04 00:37:34 +0000

    Updated InvocationLogDto

--- src/main/java/com/codility/aop/log/InvocationLogDto.java
+++ src/main/java/com/codility/aop/log/InvocationLogDto.java
@@ -1,6 +1,39 @@
 package com.codility.aop.log;
 
 import java.util.List;
+public class InvocationLogDto {
 
-public record InvocationLogDto(String className, String methodName, List<Object> args) {
+    private String className;
+    private String methodName;
+    private List<Object> args;
+
+    public InvocationLogDto(String className, String methodName, List<Object> args) {
+        this.className = className;
+        this.methodName = methodName;
+        this.args = args;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public List<Object> getArgs() {
+        return args;
+    }
+
+    public void setArgs(List<Object> args) {
+        this.args = args;
+    }
 }
