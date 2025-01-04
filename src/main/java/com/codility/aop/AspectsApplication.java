a6b61c1c08f4fcac4bbdf6a53596039926e48f7a
DanirPT <danir.hucaev@gmail.com>
2025-01-04 01:50:09 +0000

    added AspectsApplication

---
+++ src/main/java/com/codility/aop/AspectsApplication.java
@@ -0,0 +1,11 @@
+package com.codility.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AspectsApplication.class, args);
+    }
+}
