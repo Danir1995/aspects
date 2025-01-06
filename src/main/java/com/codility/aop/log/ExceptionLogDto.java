57410132f3f5ee845e474e678c6f984e3e173eb1
DanirPT <danir.hucaev@gmail.com>
2025-01-06 15:17:12 +0000

    changed ExceptionLogDto to use record instead of class

--- src/main/java/com/codility/aop/log/ExceptionLogDto.java
+++ src/main/java/com/codility/aop/log/ExceptionLogDto.java
@@ -1,18 +1,3 @@
 package com.codility.aop.log;
 
-
-import lombok.Data;
-
-@Data
-public class ExceptionLogDto {
-    private String className;
-    private String methodName;
-    private Throwable exception;
-
-    public ExceptionLogDto(String className, String methodName, Throwable exception) {
-        this.className = className;
-        this.methodName = methodName;
-        this.exception = exception;
-    }
-
-}
+public record ExceptionLogDto(String className, String methodName, Object Exception) {}
